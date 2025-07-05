bf76e25858a419f5b2e0b87c7d3807d9b30be879
amarwade <amarwade@esp.sn>
2025-07-05 00:12:28 +0000

    version 3

--- src/main/java/app/project_fin_d_etude/layout/Header.java
+++ src/main/java/app/project_fin_d_etude/layout/Header.java
@@ -12,6 +12,8 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 import app.project_fin_d_etude.utils.Routes;
 
@@ -69,8 +71,12 @@ public class Header extends HorizontalLayout {
 
             ContextMenu menu = new ContextMenu(avatar);
             menu.setOpenOnClick(true);
-            menu.addItem("Profil", e -> getUI().ifPresent(ui -> ui.getPage().setLocation("/user/profile")));
-            menu.addItem("Déconnexion", e -> getUI().ifPresent(ui -> ui.getPage().setLocation("/logout")));
+            Icon userIcon = VaadinIcon.USER.create();
+            userIcon.getStyle().set("margin-right", "8px");
+            Icon logoutIcon = VaadinIcon.SIGN_OUT.create();
+            logoutIcon.getStyle().set("margin-right", "8px");
+            menu.addItem(new HorizontalLayout(userIcon, new Span("Profil")), e -> getUI().ifPresent(ui -> ui.getPage().setLocation("/user/profile")));
+            menu.addItem(new HorizontalLayout(logoutIcon, new Span("Déconnexion")), e -> getUI().ifPresent(ui -> ui.getPage().setLocation("/logout")));
         } else {
             actionButton = new Button("Connexion", e -> getUI().ifPresent(ui -> ui.getPage().setLocation("/oauth2/authorization/keycloak")));
             actionButton.addClassNames(
